bde8cd544cbc261faeb4d15fc5568dc523784dea
NgDodo <allennguyen9192@gmail.com>
2025-11-02 18:12:38 -0600

    New

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainControl/FullTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainControl/FullTeleop.java
@@ -10,13 +10,10 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
-import java.util.List;
-
 @TeleOp(name = "Full Teleop", group = "DriveTrainControl")
-public class singleMotorTest extends OpMode {
+public class FullTeleop extends OpMode {
 
     DcMotorEx frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
     DcMotor m1, m2;
